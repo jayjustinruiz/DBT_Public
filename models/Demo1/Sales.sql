17d5a3f40ac249221773940c9368ec5114b72ed1
Jayuan Ruiz <jj.ruiz96@gmail.com>
2026-04-17 16:44:13 +0000

    New update strategies

---
+++ models/Demo1/Sales.sql
@@ -0,0 +1,21 @@
+{{ config(materialized="incremental", incremental_strategy = 'append') }}
+
+with sales_src as
+(
+    select
+    SALE_ID,
+    SALE_DATE,
+    CUSTOMER_ID,
+    PRODUCT_ID,
+    QUANTITY,
+    TOTAL_AMOUNT,
+    CREATED_AT,
+    CURRENT_TIMESTAMP AS INSERT_DTS
+    from {{source('Snowflake','SALES_SRC')}}
+
+
+    {% if is_incremental() %}
+    where CREATED_AT > (select max(INSERT_DTS) from {{this}})
+    {% endif %}
+)
+select * from sales_src
